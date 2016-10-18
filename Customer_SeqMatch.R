a70aaa05aa124e4fca52c936176d6f5847b12674
Moeen Kamali <moeen.kamali@gmail.com>
2016-10-17 19:54:39 -0500

    Update Customer_SeqMatch.R

--- Customer_SeqMatch.R
+++ Customer_SeqMatch.R
@@ -2,7 +2,7 @@
 library(sqldf)
 
 #import data to R
-SampleTest <- read.csv("C:/Users/Skamali/Desktop/R Codes/11group_test.csv", sep=",", header=TRUE)
+SampleTest <- read.csv("~/Desktop/R Codes/11group_test.csv", sep=",", header=TRUE)
 
 colnames(SampleTest)[23] <- "F0"
 
